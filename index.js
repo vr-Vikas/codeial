91cef63153dea019bb5c775d7594bf11b5019be2
vikas <gurrakotavikas@gmail.com>
2022-07-12 11:02:35 +0530

    setup mongoose schema

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const expressEjsLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
 const expressLayouts =require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
 app.use(express.static('./assets'))
 
