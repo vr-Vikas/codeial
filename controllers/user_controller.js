895b42db2d31d211a19ed1c7b4061c6b19f9e027
vikas <gurrakotavikas@gmail.com>
2022-07-16 11:44:01 +0530

    Create Sign out

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -62,3 +62,12 @@ module.exports.createSession = function(req, res){
     return res.redirect('/');
 }
 
+module.exports.destroySession = function(req, res){
+    req.logout(function(err){
+        if(err){
+            return next(err);
+        }
+    });
+
+    return res.redirect('/');
+}
