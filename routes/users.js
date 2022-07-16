7ec3fb5a9d680b5a46643b83beaa843f5ddc3c4b
vikas <gurrakotavikas@gmail.com>
2022-07-16 11:11:29 +0530

    Setting up MongoStore for session cookies

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const usersController = require('../controllers/user_controller');
 
 console.log('user router');
 
-router.get('/profile', usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
 
+// use passporrt as a middleware to authenticate
+router.post('/create-session', passport.authenticate(
+    'local',
+    {failureRedirect: '/users/sign-in'},
+), usersController.createSession);
 
 module.exports = router;
