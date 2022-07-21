8c6bdcdd12261bc968d223b81424f497f7857a75
vikas <gurrakotavikas@gmail.com>
2022-07-21 12:32:12 +0530

    Display Posts and related User

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,26 @@
 const { use } = require("../routes");
+const Post = require('../models/post');
 
 module.exports.home = function(req, res){
     // return res.end('<h1>Express is up for Codeial!</h1>');
-    console.log(req.cookies);
-    res.cookie('user_id', 25);
-    return res.render('home', {
-        title: "Home"
-    })
+    // console.log(req.cookies);
+    // res.cookie('user_id', 25);
+
+    // Post.find({}, function(err, posts){
+    //     return res.render('home', {
+    //         title: "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+    
+    // populate the user of each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts: posts
+        });
+    });
+    
 }
 
 //module.exports.actionName(function(req, res)){}
